92dfe5068e891cf61964ce20a97b16fb30c2161f
kariem <kariemgamal7777@gmail.com>
2023-03-16 14:28:22 -0700

    sad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -Print 
+ * main -Print
  *
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
