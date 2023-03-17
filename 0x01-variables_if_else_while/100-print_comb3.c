4c9e05bccf6819387bb79712fbd337460a4cb9df
kariem <kariemgamal7777@gmail.com>
2023-03-17 10:26:39 -0700

    sas

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
